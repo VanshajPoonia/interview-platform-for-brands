fc46ebc9305d3fcca912b73fe6d0f0873ec4c6cc
Vanshaj Poonia <74147463+VanshajPoonia@users.noreply.github.com>
2025-05-12 17:03:29 +0000

    Fixed typo

--- services/Constants.jsx
+++ services/Constants.jsx
@@ -90,7 +90,7 @@ export const FEEDBACK_PROMPT = `{{conversation}}
 Depends on this Interview Conversation between assitant and user, 
 Give me feedback for user interview. Give me rating out of 10 for technical Skills, 
 Communication, Problem Solving, Experience. Also give me summary in 3 lines 
-about the interview and one line to let me know whether is recommanded 
+about the interview and one line to let me know whether is recommended 
 for hire or not with msg. Give me response in JSON format
 {
     feedback:{
